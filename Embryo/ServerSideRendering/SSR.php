19d826ac2d3c5a77dc19381bcf8fadcd93ebd4e5
Davide Cesarano <Dav.Cesarano@unipegaso.local>
2019-07-24 16:12:19 +0200

    PHPDoc

--- Embryo/ServerSideRendering/SSR.php
+++ Embryo/ServerSideRendering/SSR.php
@@ -1,44 +1,95 @@
 <?php
 
     /**
-     * RequestHandler
+     * SSR
      *
-     * Create a collection of middleware,
-     * handle the request and return a response.
+     * Server side rendering JavaScript PHP application
+     * with V8Js.
      *
      * @author Davide Cesarano <davide.cesarano@unipegaso.it>
-     * @link   https://github.com/davidecesarano/embryo-middleware
-     * @see    https://github.com/php-fig/http-server-handler/blob/master/src/RequestHandlerInterface.php
+     * @link   https://github.com/davidecesarano/embryo-serversiderendering
      */
 
     namespace Embryo\ServerSideRendering;
 
     class SSR
     {
+        /**
+         * @var V8Js $v8js
+         */
         private $v8js;
+        
+        /**
+         * @var bool $enabled
+         */
         private $enabled = true;
+        
+        /**
+         * @var array $env
+         */
         private $env = [];
+        
+        /**
+         * @var string|array $entry
+         */
         private $entry;
+
+        /**
+         * @var array $context
+         */
         private $context = [];
+        
+        /**
+         * @var string $script
+         */
         private $script = '';
 
+        /**
+         * @var string $fallback
+         */
+        private $fallback = '';
+
+        /**
+         * Set V8Js engine.
+         * 
+         * @param V8Js $v8js
+         */
         public function __construct($v8js)
         {
             $this->v8js = $v8js;
         }
 
-        public function enabled(bool $enabled = true)
+        /**
+         * Enabled server side rendering.
+         * Default is true.
+         * 
+         * @param bool $enbled
+         */
+        public function enabled(bool $enabled = true): self
         {
             $this->enabled = $enabled;
             return $this;
         }
 
+        /**
+         * Set env vars.
+         * 
+         * @param array $env 
+         * @return self
+         */
         public function env(array $env): self
         {
             $this->env = $env;
             return $this;
         }
 
+        /**
+         * Set path server script\s. 
+         * 
+         * @param  string|array $entry 
+         * @return self 
+         * @throws InvalidArgumentException
+         */
         public function entry($entry): self
         {
             if (!is_string($entry) && !is_array($entry)) {
@@ -48,18 +99,48 @@
             return $this;
         }
 
+        /**
+         * Set context.
+         * 
+         * @param array $context 
+         * @return self
+         */
         public function context(array $context): self
         {
             $this->context = $context;
             return $this;
         }
 
+        /**
+         * Set Javascript script. 
+         * 
+         * @param string $script 
+         * @return self
+         */
         public function script(string $script): self 
         {
             $this->script = $script;
             return $this;
         }
 
+        /**
+         * Set fallback html when server side rendering 
+         * is disabled.
+         * 
+         * @param string $fallback 
+         * @return self
+         */
+        public function fallback(string $fallback): self
+        {
+            $this->fallback = $fallback;
+            return $this;
+        }
+
+        /**
+         * Execute and render scripts.
+         * 
+         * @return string
+         */
         public function render()
         {
             if (!$this->enabled) {
@@ -91,10 +172,12 @@
                 'var process = process || { env: {} }',
                 implode(';', $process),
                 "var context = {$context}",
-                implode(';', $entry),
-                $this->script
+                $this->script,
+                implode(';', $entry)
             ]);
-
-            return $this->v8js->executeString($javascript);
+            
+            ob_start();
+            $this->v8js->executeString($javascript);
+            return ob_get_clean();
         }
     }
